0a5f8c765f40b599e8bb6aa7f716e597e9948d3a
wenye <1583247975@qq.com>
2020-03-20 17:45:59 +0800

    feat(index.ts): 添加若干方法

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
-export function getStr() {
-  return "hello";
+/** 节流 */
+export function throttle(this: any, fn: (...args: any[]) => any, wait: number) {
+  let last: number;
+  return (...args: any[]) => {
+    const now = Date.now();
+    if (!last || now - last > wait) {
+      fn.call(this, ...args);
+      last = now;
+    }
+  };
+}
+
+/** 防抖 */
+export function debounce(this: any, fn: (...args: any[]) => any, wait: number) {
+  let timer: NodeJS.Timeout;
+  return (...args: any[]) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.call(this, ...args);
+    }, wait);
+  };
+}
+
+const RATE = Symbol("RATE");
+const START = Symbol("START");
+const END = Symbol("END");
+interface IGift {
+  [RATE]: number;
+  [START]: number;
+  [END]: number;
+}
+/** 抽奖函数 */
+export function lottery<T extends Record<string, any>>(gifts: T[], key: string = "rate"): T | null {
+  const arr: Array<IGift & T> = [];
+  let total = 0;
+  // 计算范围
+  gifts.forEach(gift => {
+    const newGift = Object.assign({ [START]: total, [END]: total, [RATE]: gift[key] }, gift);
+    total += gift[key];
+    newGift[END] = total;
+    arr.push(newGift);
+  });
+  // 随机落在那个范围
+  const random = Math.random() * total;
+  for (const gift of arr) {
+    if (gift[START] <= random && random < gift[END]) {
+      return gift;
+    }
+  }
+  return null;
 }
--- test/test-index.ts
+++ test/test-index.ts
@@ -1,8 +1,52 @@
-import { getStr } from "../src/index";
+import * as utils from "../src/index";
 import { assert } from "chai";
 
-describe("test", function() {
-  it("hello", function() {
-    assert.equal(getStr(), "hello");
+function timeout(ms: number) {
+  return new Promise((r, j) => {
+    setTimeout(r, ms);
+  });
+}
+
+describe("utils", function() {
+  it("throttle", async function() {
+    let count = 0;
+    const fn = utils.throttle(() => {
+      count++;
+    }, 15);
+    fn();
+    assert.strictEqual(count, 1);
+    fn();
+    assert.strictEqual(count, 1);
+    await timeout(20);
+    fn();
+    assert.strictEqual(count, 2);
+  });
+
+  it("debounce", async function() {
+    let count = 0;
+    const fn = utils.debounce(() => {
+      count++;
+    }, 15);
+    fn();
+    fn();
+    assert.strictEqual(count, 0);
+    await timeout(20);
+    assert.strictEqual(count, 1);
+  });
+
+  it("lottery", function() {
+    const awards = [
+      { type: 1, rate: 0 },
+      { type: 2, rate: 10 },
+      { type: 3, rate: 10 },
+    ];
+    const obj = { 1: 0, 2: 0, 3: 0 };
+    // 抽奖10000次 误差不超过500
+    for (let i = 0; i < 10000; i++) {
+      const ret = utils.lottery(awards);
+      if (ret) obj[ret.type as 2]++;
+    }
+    assert.strictEqual(obj[1], 0);
+    assert.isTrue(Math.abs(obj[2] - obj[3]) < 500);
   });
 });
